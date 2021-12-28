8271081c864ad9bdf2340fad62adab17f63bb817
Gunchar <Gunchar@DESKTOP-PUDR2K0>
2021-12-28 21:05:19 +0400

    Add project files.

---
+++ MyList/MyList.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+
+    public class MyList <T>
+    {
+        class Node<T>
+        {
+            public T Value { get; set; }
+            public Node<T> NextNode { get; set; }
+            public Node() {
+                NextNode = null;
+            }
+            public Node(T val)
+            {
+                Value = val;
+                NextNode = null;
+            }
+        }
+        public int Length { get; set; }
+        private Node<T> node;
+        public T this[int index]
+        {
+            get
+            {
+                var tmp = node;
+                int i = 0;
+                while (true)
+                {
+                    if (i == index)
+                        return tmp.Value;
+                    tmp = tmp.NextNode;
+                    i++;
+                }
+            }
+            set
+            {
+                var tmp = node;
+                int i = 0;
+                while (true)
+                {
+                    if(i == index)
+                    {
+                        tmp.Value = value;
+                        return;
+                    }
+                    tmp = tmp.NextNode;
+                    i++;
+                }
+            }
+        }
+        public MyList()
+        {
+            Length = 0;
+            node = null;
+        }
+        public MyList(params T[] arr)
+        {
+            foreach(var item in arr)
+            {
+                this.Add(item);
+            }
+        }
+        public void Add(T val)
+        {
+            Length++;
+            if(this.node == null)
+            {
+                this.node = new Node<T>(val);
+                return;
+            }
+            var tmp = this.node;
+            for (; tmp.NextNode != null; tmp = tmp.NextNode);
+            tmp.NextNode = new Node<T>(val);
+        }
+        public void Remove(int index)
+        {
+            if(index >= Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (index == 0)
+            {
+                this.node = node.NextNode;
+                Length--;
+                return;
+            }
+            int i = 1;
+            var tmp = this.node;
+            while(true)
+            {
+                if (i == index)
+                {
+                    tmp.NextNode = tmp.NextNode.NextNode;
+                    Length--;
+                    return;
+                }
+                i++;
+                tmp = tmp.NextNode;
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < Length; i++)
+                sb.Append($"{this[i].ToString()}  ");
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ MyList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var StringList = new MyList<string>("Giorgi", "Ioseliani");
+            Console.WriteLine($"The Initial StringList:\t\t\t{StringList}");
+            StringList.Add("Credo");
+            Console.WriteLine($"StringList After Adding:\t\t{StringList}");
+            StringList.Remove(1);
+            Console.WriteLine($"StringList After Remove At Index(1):\t{StringList}");
+            Console.WriteLine($"StringList[0]:\t\t\t\t{StringList[0]}");
+            Console.WriteLine(new String('-', 80));
+
+
+            var IntList = new MyList<int>(new int[] { 1, 6, 7, 2, 6, 8, 1, 2, 5, 7, 16 });
+            Console.WriteLine($"The Initial IntList:\t\t\t{IntList}");
+            IntList.Add(21);
+            Console.WriteLine($"IntList After Adding:\t\t\t{IntList}");
+            IntList.Remove(7);
+            Console.WriteLine($"IntList After Remove At Index(7):\t{IntList}");
+            Console.WriteLine($"IntList[9]:\t\t\t\t{IntList[9]}");
+            Console.WriteLine(new String('-', 80));
+        }
+
+    }
+}
